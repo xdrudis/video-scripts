536c8fb27c01c269d93600e3185cc382cab71bcc
xdrudis <xavierdrudis@yahoo.es>
2020-07-05 11:32:37 -0700

    AV1 support (#5)

--- ffmpeg-compile/build-ffmpeg.sh
+++ ffmpeg-compile/build-ffmpeg.sh
@@ -37,14 +37,18 @@ LIBVPX_VERSION=de4aeda            # git commit, as latest version v1.8.2 fail to
 LIBWEBP_VERSION=1.1.0             # https://github.com/webmproject/libwebp/releases
 LIBASS_VERSION=0.13.7             # https://github.com/libass/libass/releases
 NV_CODEC_HEADERS_VERSION=9.1.23.1 # https://github.com/FFmpeg/nv-codec-headers/releases
+LIBDAV1D_VERSION=0.7.1            # https://code.videolan.org/videolan/dav1d/-/releases
+SVT_AV1_VERSION=0.8.4             # https://github.com/OpenVisualCloud/SVT-AV1/releases
 
 OPENSSL=/usr/local/opt/openssl@1.1 # Needed for Mac OSX. No-op for the rest
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/lib/$HOSTTYPE-$OSTYPE/pkgconfig:$PREFIX/lib64/pkgconfig:${OPENSSL}/lib/pkgconfig:${PKG_CONFIG_PATH:-} # https://stackoverflow.com/a/29792635
-export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/lib64:${LD_LIBRARY_PATH:-}
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:${OPENSSL}/lib/pkgconfig:${PKG_CONFIG_PATH:-} # https://stackoverflow.com/a/29792635
+export LD_LIBRARY_PATH=$PREFIX/lib:${LD_LIBRARY_PATH:-}
 PATH="${PREFIX}/bin:$HOME/.local/bin:$PATH"
 
 ncores=$(nproc 2> /dev/null || sysctl -n hw.ncpu 2> /dev/null || echo 4)
 njobs=$(( $ncores * 3 / 2 )) # 1.5 number of cores
+export MAKEFLAGS="-j$njobs"
+export MAKEOPTS="-j$njobs"
 
 TMPDIR=$(mktemp -d)
 
@@ -65,10 +69,32 @@ else
    DIR=$TMPDIR/nasm; mkdir -p "$DIR"; cd "$DIR"
    curl -sL https://www.nasm.us/pub/nasm/releasebuilds/${NASM_VERSION}/nasm-${NASM_VERSION}.tar.gz | tar xz --strip-components=1
    ./configure --prefix="$PREFIX"
-   make -j$njobs
+   make
    $sudo make install
    rm -fR "$DIR"
 fi
+
+#
+# dav1d
+#
+DIR=$TMPDIR/dav1d; cd "$TMPDIR"
+git clone -b ${LIBDAV1D_VERSION} https://code.videolan.org/videolan/dav1d.git
+cd $DIR
+meson build --prefix "$PREFIX" --libdir "$PREFIX/lib" --buildtype release
+$sudo ninja -vC build install
+rm -fR "$DIR"
+
+#
+# SVT-AV1
+#
+DIR=$TMPDIR/svt-av1; mkdir -p "$DIR"; cd "$DIR"
+curl -sL https://github.com/OpenVisualCloud/SVT-AV1/archive/v${SVT_AV1_VERSION}.tar.gz | tar xz --strip-components=1
+mkdir -p Bin/Release
+cd Build/linux
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="$PREFIX"/lib -DCMAKE_ASM_NASM_COMPILER=nasm ../..
+make
+$sudo make install
+rm -fR "$DIR"
 
 #
 # libass
@@ -77,7 +103,7 @@ DIR=$TMPDIR/libass; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://github.com/libass/libass/archive/${LIBASS_VERSION}.tar.gz | tar xz --strip-components=1
 ./autogen.sh
 ./configure --prefix="$PREFIX" --disable-static --enable-shared
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -88,7 +114,7 @@ DIR=$TMPDIR/fdk-aac; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://github.com/mstorsjo/fdk-aac/archive/v${FDKAAC_VERSION}.tar.gz | tar xz --strip-components=1
 autoreconf -fiv
 ./configure --prefix="$PREFIX" --enable-shared --datadir="$DIR"
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -99,7 +125,7 @@ DIR=$TMPDIR/kvazaar; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://github.com/ultravideo/kvazaar/archive/v${KVAZAAR_VERSION}.tar.gz | tar xz --strip-components=1
 ./autogen.sh
 ./configure --prefix="$PREFIX" --disable-static --enable-shared
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -109,7 +135,7 @@ if [[ "$HOSTTYPE" == x86_64 ]] ; then
    #
    DIR=$TMPDIR/vmaf; mkdir -p "$DIR"; cd "$DIR"
    curl -sL https://github.com/Netflix/vmaf/archive/v${LIB_VMAF_VERSION}.tar.gz | tar xz --strip-components=1
-   meson setup libvmaf libvmaf/build --buildtype release --prefix="$PREFIX"
+   meson setup libvmaf libvmaf/build --buildtype release --prefix="$PREFIX" --libdir "$PREFIX/lib"
    $sudo ninja -vC libvmaf/build install
    rm -fR "$DIR"
 
@@ -125,7 +151,7 @@ DIR=$TMPDIR/libmp3lame; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://downloads.sourceforge.net/project/lame/lame/${LIBMP3LAME_VERSION}/lame-${LIBMP3LAME_VERSION}.tar.gz | tar xz --strip-components=1
 [[ "$OSTYPE" == darwin* ]] && cp include/libmp3lame.sym include/libmp3lame.sym.bak && sed '/lame_init_old/d' include/libmp3lame.sym.bak > include/libmp3lame.sym # see https://stackoverflow.com/a/53955675
 ./configure --prefix="$PREFIX" --enable-shared --enable-nasm
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -137,7 +163,7 @@ curl -sL https://github.com/uclouvain/openjpeg/archive/v${OPENJPEG_VERSION}.tar.
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -147,7 +173,7 @@ rm -fR "$DIR"
 DIR=$TMPDIR/libopus; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://archive.mozilla.org/pub/opus/opus-${LIBOPUS_VERSION}.tar.gz | tar xz --strip-components=1
 ./configure --prefix="$PREFIX" --enable-shared
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -157,7 +183,7 @@ rm -fR "$DIR"
 DIR=$TMPDIR/x264; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://code.videolan.org/videolan/x264/-/archive/master/x264-${X264_VERSION}.tar.gz | tar xz --strip-components 1
 ./configure --prefix="$PREFIX" --enable-static --enable-pic
-make -j$njobs
+make
 $sudo make install-lib-static
 rm -fR "$DIR"
 
@@ -168,7 +194,7 @@ DIR=$TMPDIR/x265; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://github.com/videolan/x265/archive/${X265_VERSION}.tar.gz | tar xz --strip-components 1
 cd build/linux
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ../../source
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -178,7 +204,7 @@ rm -fR "$DIR"
 DIR=$TMPDIR/libvpx; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://github.com/webmproject/libvpx/tarball/${LIBVPX_VERSION} | tar xz --strip-components 1
 ./configure --prefix="$PREFIX" --as=nasm --disable-dependency-tracking --disable-examples --disable-unit-tests --enable-pic --enable-vp9-highbitdepth
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -188,7 +214,7 @@ rm -fR "$DIR"
 DIR=$TMPDIR/libwebp; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${LIBWEBP_VERSION}.tar.gz | tar xz --strip-components 1
 ./configure --prefix="$PREFIX" --enable-libwebpdecoder --enable-libwebpdemux --enable-libwebpmux 
-make -j$njobs
+make
 $sudo make install
 rm -fR "$DIR"
 
@@ -199,7 +225,7 @@ if [ -r /usr/local/cuda ] ; then
    DIR=$TMPDIR/nv-codec-headers; mkdir -p "$DIR"; cd "$DIR"
    curl -sL https://github.com/FFmpeg/nv-codec-headers/archive/n${NV_CODEC_HEADERS_VERSION}.tar.gz | tar xz --strip-components 1
    cp Makefile Makefile.bak ; sed "s;/usr/local;$PREFIX;" Makefile.bak > Makefile
-   make -j$njobs
+   make
    $sudo make install
    rm -fR "$DIR"
    CUDA="--enable-nvenc --enable-cuda --enable-cuvid --enable-libnpp"
@@ -214,6 +240,7 @@ fi
 #
 DIR=$TMPDIR/ffmpeg; mkdir -p "$DIR"; cd "$DIR"
 curl -sL https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.gz | tar xz --strip-components=1
+curl -sL https://raw.githubusercontent.com/OpenVisualCloud/SVT-AV1/v${SVT_AV1_VERSION}/ffmpeg_plugin/0001-Add-ability-for-ffmpeg-to-run-svt-av1.patch | patch -p1
 ./configure \
    --disable-debug \
    --disable-doc \
@@ -243,11 +270,13 @@ curl -sL https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.gz | tar xz --
    --enable-libwebp \
    --enable-libass \
    --enable-fontconfig \
+   --enable-libdav1d \
+   --enable-libsvtav1 \
    --extra-cflags="-I${PREFIX}/include -I${PREFIX}/include/ffnvcodec -I/usr/local/cuda/include/" \
    --extra-ldflags="-L${PREFIX}/lib -L${OPENSSL}/lib -L/usr/local/cuda/lib64" \
    --extra-libs="-ldl -lm" \
    --prefix="$PREFIX" || ( cat ffbuild/config.log ; exit 1 )
-make -j$njobs
+make
 $sudo make install
 cd tools
 make qt-faststart
@@ -261,5 +290,5 @@ echo 🎉🎉🎉 Success!
 echo
 echo "Please run this:
 
-echo 'export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/lib64:$PREFIX/lib/$HOSTTYPE-$OSTYPE$CUDA_LD_LIBRARY_PATH:\$LD_LIBRARY_PATH
+echo 'export LD_LIBRARY_PATH=$PREFIX/lib$CUDA_LD_LIBRARY_PATH:\$LD_LIBRARY_PATH
 export PATH=$PREFIX/bin:\$PATH' >> $HOME/.bashrc"
